0504b84c43dc80e928fb0cd59d013834c07d5c9b
Kaylee Driscoll <kaylee.driscoll@Kaylees-MacBook-Air.local>
2025-11-03 21:47:18 -0500

    Added hospital scoring and user preference header file, updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "hospital.h"
 #include "parsing.h"
+#include "user_prefs.h"
 #include <vector>
+#include <string>
+#include <iomanip>
 #include <unordered_map>
 #include <iostream>
+
+void compute(std::vector<Hospital> &hospitals, double userlat, double userlon, const UserPreferences &user_prefs);
+
 
 void assignCoordinates(std::vector<Hospital>& hospitals,
                        const std::unordered_map<std::string, std::pair<double, double>>& coords)
@@ -17,4 +23,11 @@ void assignCoordinates(std::vector<Hospital>& hospitals,
         }
     }
     std::cout << "Coordinates matched for " << matched << " of " << hospitals.size() << " hospitals.\n";
+}
+
+int main() {
+  std::vector<Hospital> hospitals = parseHospitalCSV("hospitals.csv");
+  std::unordered_map<std::string, std::pair<double, double>> coords;
+  assignCoordinates(hospitals, coords);
+  return 0;
 }
